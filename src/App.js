05177ccc922c4aa0d13cba26be9a597ad719defe
Tandem Seven <tandemseven@Note-337.local>
2019-06-11 11:55:06 -0400

    Quiz Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react'
-import './App.css'
+import React, { Component } from "react";
+import { ThemeProvider } from "@material-ui/styles";
+import "./App.css";
+import Quiz from "./Quiz";
+// const theme = {
+//   background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)"
+// };
 
 class App extends Component {
   render() {
-    return (
-      <div />
-    )
+    return <Quiz />;
   }
 }
 
-export default App
+export default App;
---
+++ src/Quiz.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { useTheme, makeStyles } from "@material-ui/styles";
+let quizData = require("./quiz_data.json");
+
+class Quiz extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { quiz_position: 1 };
+  }
+  render() {
+    return (
+      <div>
+        <h1 className="QuizQuestion">
+          {quizData.quiz_questions[0].instruction_text}
+        </h1>
+      </div>
+    );
+  }
+}
+export default Quiz;
